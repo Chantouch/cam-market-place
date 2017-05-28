2d434b0f209fc19c5f214d4da0eaf26e911a489b
Theary RIN <rin.theary@gmail.com>
2017-05-28 19:46:58 +0700

    add category in single

--- app/Http/Controllers/SingleController.php
+++ app/Http/Controllers/SingleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Category;
 
 class SingleController extends Controller
 {
@@ -23,8 +24,9 @@ class SingleController extends Controller
      */
     public function index()
     {
+        $categories = Category::where('status',1)->get();
 
-       return view('front.pages.view_product');
+       return view('front.pages.view_product',compact('categories'));
     }
 
     /**
